c5b3dd9cf8ed1f780133d19f19ef3c9eb6f37788
Eddie <edrismoavenghafouri@gmail.com>
2024-09-10 20:33:53 +0330

    adding 392.Is Subsequence

---
+++ 1392_Is_Subsequence/Program.cs
@@ -0,0 +1,26 @@
+﻿//"abc", t = "ahbgdc" true
+//"axc", t = "ahbgdc" false
+
+var s = "axc";
+var t = "ahbgdc";
+
+var output = IsSubsequence(s, t);
+Console.WriteLine(1);
+
+
+static bool IsSubsequence(string s, string t)
+{
+    var i = 0;
+    int j = 0;
+    while (j < t.Length)
+    {
+        if (i < s.Length && s[i] == t[j])
+        {
+            i++;
+        }
+
+        j++;
+    }
+
+    return i == s.Length;
+}
