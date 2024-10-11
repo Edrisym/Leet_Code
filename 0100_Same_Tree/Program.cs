610354a779fd8f0783a76fcf15c559e004903c1e
Eddie <edrismoavenghafouri@gmail.com>
2024-10-11 22:58:26 +0330

    adding solution same tree

---
+++ 0100_Same_Tree/Program.cs
@@ -0,0 +1,35 @@
+﻿// For tree 'p'
+
+using System.Runtime.CompilerServices;
+
+TreeNode p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+
+// For tree 'q'
+TreeNode q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+
+
+Solution.IsSameTree(p, q);
+
+
+public class Solution
+{
+    public static bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        var isSame = false;
+        List<int> nodes = new List<int>();
+
+        PreOrder(p, nodes);
+        PreOrder(p, nodes);
+
+        return isSame;
+    }
+
+    static void PreOrder(TreeNode node, List<int> nodes)
+    {
+        if (node == null) return;
+
+        nodes.Add(node.val);
+        PreOrder(node.left, nodes);
+        PreOrder(node.right, nodes);
+    }
+}
