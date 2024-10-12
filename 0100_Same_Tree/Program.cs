9e3e2bfb622eaa98caaf033a6d6e57572918dde1
Eddie <edrismoavenghafouri@gmail.com>
2024-10-12 22:34:47 +0330

    Completed

--- 0100_Same_Tree/Program.cs
+++ 0100_Same_Tree/Program.cs
@@ -10,36 +10,29 @@ TreeNode q = new TreeNode(1, null, new TreeNode(2));
 var s = Solution.IsSameTree(p, q);
 Console.WriteLine(s);
 
-
-public class Solution
+public static class Solution
 {
     public static bool IsSameTree(TreeNode p, TreeNode q)
     {
-        var isSame = false;
         StringBuilder pNodes = new StringBuilder();
         StringBuilder qNodes = new StringBuilder();
 
-        Console.WriteLine(pNodes);
-        Console.WriteLine(qNodes);
-
         PreOrder(p, pNodes);
         PreOrder(q, qNodes);
-
-        if (qNodes.Equals(pNodes))
-        {
-            isSame = true;
-        }
-
-        return isSame;
+        
+        return pNodes.ToString().Equals(qNodes.ToString());
     }
 
     static void PreOrder(TreeNode node, StringBuilder nodes)
     {
-        if (node == null){}
+        if (node == null)
+        {
+            nodes.Append("#,");
+            return;
+        }
 
-        nodes.Append(node.val);
-
-        PreOrder(node.left, nodes);
+        nodes.Append(node.val + ",");
+        PreOrder(node.left, nodes); 
         PreOrder(node.right, nodes);
     }
 }
