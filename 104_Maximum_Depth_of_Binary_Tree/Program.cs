3811e178ed273e4e2d57281d6d846b23997b41d6
Edris <moaven.edris@okala.com>
2025-10-21 00:04:33 +0330

    added 104_Maximum_Depth_of_Binary_Tree

---
+++ 104_Maximum_Depth_of_Binary_Tree/Program.cs
@@ -0,0 +1,37 @@
+﻿using _104_Maximum_Depth_of_Binary_Tree;
+using LeetCode_Models;
+
+var root = new TreeNode(3,
+    left: new TreeNode(9),
+    right: new TreeNode(20, right: new TreeNode(15), left: new TreeNode(7)));
+
+var maxDepth = Solution.MaxDepth(root);
+Console.WriteLine(maxDepth);
+
+namespace _104_Maximum_Depth_of_Binary_Tree
+{
+    public static class Solution
+    {
+        public static int MaxDepth(TreeNode? root)
+        {
+            if (root == null) return 0;
+
+            int leftDepth = MaxDepth(root.left);
+            int rightDepth = MaxDepth(root.right);
+
+            return 1 + Math.Max(leftDepth, rightDepth);
+        }
+    }
+}
+
+//Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+// Example 2:
+// 
+// Input: root = [1,null,2]
+// Output: 2
+
+//Constraints:
+// 
+// The number of nodes in the tree is in the range [0, 104].
+// -100 <= Node.val <= 100
